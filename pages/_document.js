71965beb2f646acdb3631ec26932c999eada8482
Your Name <you@example.com>
2026-04-10 17:47:35 +0200

    fix icons

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,10 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=optional"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
