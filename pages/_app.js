52f5639d8cc309a5e2d75b67f7d859fe1f417241
Your Name <you@example.com>
2026-04-10 18:18:08 +0200

    fix issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,36 @@
 import "@/styles/globals.css";
 
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 import { HomesProvider } from "@/context/HomesContext";
 import { PropertiesProvider } from "@/context/PropertiesContext";
 import { SlidesProvider } from "@/context/ImagesContext";
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
+  const router = useRouter();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  useEffect(() => {
+    const handleRouteChangeComplete = () => {
+      requestAnimationFrame(() => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "auto",
+        });
+      });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+    };
+  }, [router]);
 
   return (
     <HomesProvider>
